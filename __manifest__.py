490990a3e64dec58b0f3d7135fc70a757c1f42d6
Andres Sorto <andresismaelsortozuniga@gmail.com>
2022-02-22 20:55:49 -0600

    Creacion de curso en open academy

--- __manifest__.py
+++ __manifest__.py
@@ -24,10 +24,10 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'views/course.xml'
+        'views/course.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
